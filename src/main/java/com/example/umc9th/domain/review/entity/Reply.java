ffc8351087ae51540695071df5f0468bed3d6b3a
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-05 21:37:22 +0900

    feat: 4주차 미션 Review, ReviewPhoto, Reply 엔터티 생성 #1

---
+++ src/main/java/com/example/umc9th/domain/review/entity/Reply.java
@@ -0,0 +1,20 @@
+package com.example.umc9th.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "reply")
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
---
+++ src/main/java/com/example/umc9th/domain/review/entity/ReviewPhoto.java
@@ -0,0 +1,20 @@
+package com.example.umc9th.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "review_photo")
+public class ReviewPhoto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "photo_url")
+    private String photo_url;
+}
