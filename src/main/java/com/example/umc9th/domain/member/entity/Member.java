fa360e0c132af963f9ca1c5dc64b501fd9dfee4c
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-05 18:08:21 +0900

    feat: 4주차 미션 Memebr, Food, Term 엔터티 작성 #1

---
+++ src/main/java/com/example/umc9th/domain/member/entity/Member.java
@@ -0,0 +1,59 @@
+package com.example.umc9th.domain.member.entity;
+
+import com.example.umc9th.domain.member.enums.Address;
+import com.example.umc9th.domain.member.enums.Gender;
+import com.example.umc9th.domain.member.enums.SocialType;
+import com.example.umc9th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", length = 3, nullable = false)
+    private String name;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Gender gender = Gender.NONE;
+
+    @Column(name = "birth", nullable = false)
+    private LocalDate birth;
+
+    @Column(name = "address", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Address address;
+
+    @Column(name = "detail_address", nullable = false)
+    private String detail_address;
+
+    @Column(name = "social_uid", nullable = false)
+    private String social_uid;
+
+    @Column(name = "social_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SocialType social_type;
+
+    @Column(name = "point", nullable = false)
+    private Integer point;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phone_number;
+
+}
