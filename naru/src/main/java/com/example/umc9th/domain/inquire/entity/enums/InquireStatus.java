d7bc5e259166d11f7f5014324eee250895ed306d
yangjiae12 <yangjiae12@gmail.com>
2025-10-07 19:40:58 +0900

    feat: 4주차 미션 공통 BaseEntity 및 enum 정의 #1

---
+++ naru/src/main/java/com/example/umc9th/domain/inquire/entity/enums/InquireStatus.java
@@ -0,0 +1,11 @@
+package com.example.umc9th.domain.inquire.entity.enums;
+
+public enum InquireStatus {
+    RECEIVED,
+    IN_PROGRESS,
+    WAITING_FOR_CUSTOMER,
+    RESOLVED,
+    CLOSED,
+    ESCALATED,
+    CANCELED
+}
---
+++ naru/src/main/java/com/example/umc9th/domain/inquire/entity/enums/InquireType.java
@@ -0,0 +1,10 @@
+package com.example.umc9th.domain.inquire.entity.enums;
+
+public enum InquireType {
+    STORE,
+    ACCOUNT,
+    PROMOTION,
+    SYSTEM,
+    SUGGESTION,
+    ETC
+}
