e9d2763de341e315820c1b5e4411066e24447136
yangjiae12 <yangjiae12@gmail.com>
2025-10-11 01:46:24 +0900

    feat: 도메인별 Repository 파일 추가 #8

---
+++ naru/src/main/java/com/example/umc9th/domain/review/repository/ReviewPhotoRepository.java
@@ -0,0 +1,8 @@
+package com.example.umc9th.domain.review.repository;
+
+import com.example.umc9th.domain.review.entity.ReviewPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewPhotoRepository extends JpaRepository<ReviewPhoto, Long> {
+
+}
