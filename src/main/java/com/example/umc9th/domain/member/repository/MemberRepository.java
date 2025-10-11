16318036402ced9789f7bdfed5edbe9be95a84ea
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-11 19:05:35 +0900

    feat: 5주차 미션 마이페이지 쿼리, MyPageDto 구현 #9

--- src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
@@ -1,7 +1,16 @@
 package com.example.umc9th.domain.member.repository;
 
+import com.example.umc9th.domain.member.dto.MyPageDto;
 import com.example.umc9th.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("SELECT new com.example.umc9th.domain.member.dto.MyPageDto(m.id, m.email, m.phone_number, m.point) " +
+            "FROM Member m WHERE m.id = :id")
+    Optional<MyPageDto> findCurrentMember(@Param("id") Long id);
 }
