9fbe33ba9e456822f4dc2da142a076835e911cf8
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-11 19:05:35 +0900

    feat: 홈 화면 쿼리, MyPageDto 구현 #9

---
+++ src/main/java/com/example/umc9th/domain/member/dto/MyPageDto.java
@@ -0,0 +1,31 @@
+package com.example.umc9th.domain.member.dto;
+
+public class MyPageDto {
+    private Long id;
+    private String email;
+    private String phone_number;
+    private Integer points;
+
+    public MyPageDto(Long id, String email, String phone_number, Integer points) {
+        this.id = id;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.points = points;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+}
