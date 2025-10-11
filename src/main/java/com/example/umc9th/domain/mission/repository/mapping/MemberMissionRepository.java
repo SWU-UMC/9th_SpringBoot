7b1d13c2006755f997ccc1ea1b7fe77fd834e3cf
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-11 22:06:16 +0900

    feat 진행중/진행완료 미션 쿼리, MemberMissionDto 구현 #9

--- src/main/java/com/example/umc9th/domain/mission/repository/mapping/MemberMissionRepository.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/mapping/MemberMissionRepository.java
@@ -1,7 +1,22 @@
 package com.example.umc9th.domain.mission.repository.mapping;
 
 import com.example.umc9th.domain.mission.entity.mapping.MemberMission;
+import com.example.umc9th.domain.mission.dto.MemberMissionDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+
+    @Query("SELECT new com.example.umc9th.domain.mission.dto.MemberMissionDto(s.name, m.point, m.created_at, m.conditional, mm.status)" +
+            "FROM MemberMission mm JOIN mm.mission m  JOIN m.store s" +
+            " WHERE mm.member.id = :memberId" +
+            "  AND mm.status = :status" +
+            " ORDER BY mm.id DESC")
+    Page<MemberMissionDto> findUserMissionByStatus(
+            @Param("memberId") Long memberId,
+            @Param("status") String status,
+            Pageable pageable);
 }
