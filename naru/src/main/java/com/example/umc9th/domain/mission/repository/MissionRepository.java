fea3d70032e0b3db4491492d9259f02c3cac5cd1
yangjiae12 <yangjiae12@gmail.com>
2025-11-04 14:42:07 +0900

    chore: MissionRepository 필요없는 코드 삭제 #20

--- naru/src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
+++ naru/src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
@@ -10,30 +10,4 @@ import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
-    // 도전 가능한 미션 조회
-
-    // 1) 메서드 이름 기반 쿼리
-    @EntityGraph(attributePaths = {"store", "store.food"})
-    Slice<Mission> findByStoreAndLocation(
-            Long locationId,
-            Long lastId,
-            Pageable pageable
-    );
-
-    // 2) @Query 기반 쿼리
-    @Query("""
-        SELECT m
-        FROM Mission m
-        JOIN FETCH m.store s
-        JOIN FETCH s.food f
-        WHERE s.location.id = :locationId
-          AND m.id > :lastId
-        ORDER BY m.id ASC
-        """)
-    Slice<Mission> findMissionsByLocation(
-            @Param("locationId") Long locationId,
-            @Param("lastId") Long lastId,
-            Pageable pageable
-    );
-
 }
