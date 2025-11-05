c44010bf8caba8a9d5f87b6f299b36251e66397b
seoyoon lee <leeseoyoon01@gmail.com>
2025-11-05 12:47:47 +0900

    feat: 7주차 미션 실패 응답 통일

---
+++ leeseo/src/main/java/com/example/leeseo/global/entity/apiPayload/code/GeneralErrorCode.java
@@ -0,0 +1,30 @@
+package com.example.leeseo.global.entity.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GeneralErrorCode implements BaseErrorCode{
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST,
+            "COMMAND400_1",
+            "잘못된 요청입니다."),
+
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,
+                 "AUTH401_1",
+                    "인증이 필요합니다."),
+
+    FORBIDDEN(HttpStatus.FORBIDDEN,
+                "AUTH403_1",
+                "요청이 거부되었습니다."),
+
+    NOT_FOUND(HttpStatus.NOT_FOUND,
+            "COMMAND404_1",
+            "요청한 리소스를 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
