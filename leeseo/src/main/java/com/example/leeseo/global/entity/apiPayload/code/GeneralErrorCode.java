d64690c498803c385214465c417244300b0a7ff9
seoyoon lee <leeseoyoon01@gmail.com>
2025-11-05 14:40:19 +0900

    feat: 7주차 미션 enum에 서버에러 추가

--- leeseo/src/main/java/com/example/leeseo/global/entity/apiPayload/code/GeneralErrorCode.java
+++ leeseo/src/main/java/com/example/leeseo/global/entity/apiPayload/code/GeneralErrorCode.java
@@ -22,7 +22,12 @@ public enum GeneralErrorCode implements BaseErrorCode{
 
     NOT_FOUND(HttpStatus.NOT_FOUND,
             "COMMAND404_1",
-            "요청한 리소스를 찾을 수 없습니다.");
+            "요청한 리소스를 찾을 수 없습니다."),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "COMMON500_1",
+            "예기치 않은 서버 에러가 발생했습니다."),
+    ;
 
     private final HttpStatus status;
     private final String code;
