0f15120c73db5b8276b9be5fd6266ffc9022aed4
seoyoon lee <leeseoyoon01@gmail.com>
2025-11-15 10:28:11 +0900

    feat: 8주차 미션 - validation 추가

--- leeseo/src/main/java/com/example/leeseo/domain/review/dto/ReviewReqDTO.java
+++ leeseo/src/main/java/com/example/leeseo/domain/review/dto/ReviewReqDTO.java
@@ -3,6 +3,8 @@ package com.example.leeseo.domain.review.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
@@ -11,8 +13,10 @@ public class ReviewReqDTO {
     @Schema(name = "ReviewRequest")
     public record JoinDTO(
         @NotBlank
+        @Size(min = 1, max = 80)
         String content,
         @NotNull
+        @Range(min = 0, max = 5)
         float rate,
         @NotNull
         List<String> img_url
