5eeea5367f6964da44e372f05661e08221a63c11
sseungeun <iseungeun970@gmail.com>
2025-11-17 15:51:47 +0900

    feat: api 응답 통일

--- umc/src/main/java/com/example/umc/global/apiPayload/ApiResponse.java
+++ umc/src/main/java/com/example/umc/global/apiPayload/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.example.umc.global.apiPayload;
 
 import com.example.umc.global.apiPayload.code.BaseErrorCode;
+import com.example.umc.global.apiPayload.code.BaseSuccessCode;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
@@ -24,7 +25,9 @@ public class ApiResponse<T> {
     private T result;
 
     // 성공한 경우 (result 포함)
-
+    public static <T> ApiResponse<T> onSuccess(BaseSuccessCode code, T result) {
+        return new ApiResponse<>(true, code.getCode(), code.getMessage(), result);
+    }
     // 실패한 경우 (result 포함)
     public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
         return new ApiResponse<>(false, code.getCode(), code.getMessage(), result);
