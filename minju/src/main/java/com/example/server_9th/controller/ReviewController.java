8283143e832271084fe94227695bccb05c1bccf6
SungMinju <minju2928@naver.com>
2025-11-18 08:21:45 +0900

    refactor: controller 응답 통일적용

--- minju/src/main/java/com/example/server_9th/controller/ReviewController.java
+++ minju/src/main/java/com/example/server_9th/controller/ReviewController.java
@@ -1,10 +1,13 @@
 package com.example.server_9th.controller;
 
+import com.example.server_9th.apiPayload.ApiResponse;
+import com.example.server_9th.apiPayload.code.SuccessCode;
 import com.example.server_9th.converter.ReviewConverter;
 import com.example.server_9th.domain.mapping.review.Review;
 import com.example.server_9th.dto.ReviewDto;
 import com.example.server_9th.service.ReviewQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,21 +23,18 @@ public class ReviewController {
     private final ReviewQueryService reviewQueryService;
 
     @GetMapping("/search")
-    public List<Review> searchReview(
-            @RequestParam String query,
-            @RequestParam String type
-    ){
+    public ResponseEntity<ApiResponse<List<Review>>> searchReview(@RequestParam String query, @RequestParam String type){
         // 서비스에게 요청
         List<Review> result = reviewQueryService.searchReview(query,type);
-        return  result;
+        return  ResponseEntity.ok(ApiResponse.onSuccess(SuccessCode._OK, result));
     }
 
     @GetMapping("/myReview")
-    public List<ReviewDto.MyReviewResponseDto> getMyReviews(
+    public ResponseEntity<ApiResponse<List<ReviewDto.MyReviewResponseDto>>> getMyReviews(
             @RequestParam Long memberId,
             @RequestParam(required = false) String storeName,
             @RequestParam(required = false) Double rating
     ){
-        return reviewQueryService.getMyReviews(memberId, storeName, rating);
+        return ResponseEntity.ok(ApiResponse.onSuccess(SuccessCode._OK,reviewQueryService.getMyReviews(memberId, storeName, rating)));
     }
 }
