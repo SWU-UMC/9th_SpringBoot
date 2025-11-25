31ee178a13d3c35ec04f03424c2963aaa095ef95
sseungeun <iseungeun970@gmail.com>
2025-11-25 12:51:12 +0900

    fix: 8주차 피드백 수정

--- ruma/src/main/java/com/example/umc/domain/review/dto/req/ReviewReqDTO.java
+++ ruma/src/main/java/com/example/umc/domain/review/dto/req/ReviewReqDTO.java
@@ -1,12 +1,18 @@
 package com.example.umc.domain.review.dto.req;
 
 import java.util.List;
+import jakarta.validation.constraints.*;
 
 public class ReviewReqDTO {
 
     public record CreateDTO(
+            @NotNull
             Long storeId,
+            @NotNull(message = "rating은 필수 값입니다.")
+            @Min(value = 0)
+            @Max(value = 5)
             Integer rating,
+            @NotBlank
             String content,
             List<String> photoUrls
     ) {}
