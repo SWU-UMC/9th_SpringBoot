bf420d8585fef31e5c2e67308037f5dd24e8843f
yangjiae12 <yangjiae12@gmail.com>
2025-12-02 00:57:25 +0900

    feat: 로그인 실패 에러 코드 정의 #50
    - UserErrorCode: 비밀번호 불일치 시 사용할 PASSWORD_NOT_MATCH(401) 에러 코드 추가

--- naru/src/main/java/com/example/umc9th/domain/user/error/UserErrorCode.java
+++ naru/src/main/java/com/example/umc9th/domain/user/error/UserErrorCode.java
@@ -11,6 +11,7 @@ public enum UserErrorCode implements BaseErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER4001", "요청하신 사용자를 찾을 수 없습니다."),
     USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "USER4002", "이미 존재하는 이메일입니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "USER4003", "비밀번호가 일치하지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
